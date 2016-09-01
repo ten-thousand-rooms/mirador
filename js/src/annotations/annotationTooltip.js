5679fbaf24ea9f098270cd001b962bfdffb32a77
Seong-June Kim <seong-june.kim@yale.edu>
2016-09-01 16:13:58 -0400

    Do not show tags on tooltips -- to cumbersome.

--- js/src/annotations/annotationTooltip.js
+++ js/src/annotations/annotationTooltip.js
@@ -453,9 +453,11 @@
       '<p>{{{annoText}}}</p>',
       '</div>',
       '<div id="tags-viewer-{{windowId}}" class="tags-viewer">',
+      /* XXX seong - don't wanna show tags
       '{{#each tags}}',
       '<span class="tag">{{this}}</span>',
       '{{/each}}',
+      */
       '</div>',
       '</div>',
       '{{/each}}',
