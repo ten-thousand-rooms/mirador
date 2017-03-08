bf46443f9cac38a82edb101655765625521d14b9
Seong-June Kim <seong-june.kim@yale.edu>
2017-03-08 16:37:06 -0500

    Lower zoom rate

--- js/src/utils/openSeadragon.js
+++ js/src/utils/openSeadragon.js
@@ -11,11 +11,13 @@
         defaultZoomLevel: 0,
         blendTime:        0.1,
         alwaysBlend:      false,
-        showNavigationControl: false
+        showNavigationControl: false,
+        zoomPerClick: 1.2, // XXX seong default: 2.0
+        zoomPerScroll: 1.1 // XXX seong default: 1.2
       }, options)
 
     );
-    
+
     return osd;
 
   };
